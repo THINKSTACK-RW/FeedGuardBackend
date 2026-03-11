0c6353327801af35a8133698540cd47f2f0c1c69
christiannshuti <chrisnshuti943@gmail.com>
2026-03-11 20:21:58 +0200

    Modified citizen model

--- src/models/Citizen.js
+++ src/models/Citizen.js
@@ -5,9 +5,47 @@ module.exports = (sequelize, DataTypes) => {
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password_hash: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+    },
     phone_hash: {
       type: DataTypes.TEXT,
+      allowNull: true,
       unique: true,
+    },
+    village: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    sector: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    district: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    household_size: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     location_id: {
       type: DataTypes.UUID,
